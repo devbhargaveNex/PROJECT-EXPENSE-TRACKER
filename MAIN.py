f6d0eb242b4dd39808ad4314d638154183cfd21e
devbhargaveNex <devbhargav3002@gmail.com>
2026-06-27 09:52:41 +0530

    Delete Feature Added

--- MAIN.py
+++ MAIN.py
@@ -1,6 +1,7 @@
 choice = 0
 expenses = []
 def add(e):
+    e = e.rstrip('\n')
     exp = e.split(',')
     expense1 = expense(exp[0],exp[1],exp[2],exp[3])
     expenses.append(expense1)
@@ -20,10 +21,9 @@ with open('expenses.txt','r') as f:
 
 while choice != '6':
 
-    print('''1.ADD NEW EXPENSE
+    print('''   1.ADD NEW EXPENSE
     2.VIEW EXPENSES
     3.DELETE EXPENSES
-    4.SEARCH EXPENSES
     5.SAVE CHANGES
     6.QUIT APPLICATION''')
     choice = input('enter 1-6 for choice: ')
@@ -34,6 +34,8 @@ while choice != '6':
         for i in expenses:
             print(f'amount={i.amount} category={i.category} description={i.description} date={i.date}')
     elif choice == '5':
+        with open('expenses.txt','w') as f:
+            f.write('')
         for i in expenses:
             with open('expenses.txt','a') as f:
                 f.write(i.amount)
@@ -44,4 +46,15 @@ while choice != '6':
                 f.write(',')
                 f.write(i.date)
                 f.write('\n')
-    
+    elif choice == '3':
+        dele = input('enter the expense to delete: ')
+        dels = dele.split(',')
+        n = -1
+        for i in expenses:
+            n += 1
+            if i.amount == dels[0]:
+                if i.category == dels[1]:
+                    if i.description == dels[2]:
+                        if i.date == (dels[3]):
+                            rm = expenses.pop(n)
+                            print(rm)
